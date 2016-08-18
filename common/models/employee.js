e3c96f92529037e8b41fa3df736a70b60c68fcb1
Muhammad Zulfikar <fikarisasi@gmail.com>
2016-08-18 14:18:55 +0700

    add remote method getName to path /getName set ACL: - everyone can see other Employee name

--- common/models/employee.js
+++ common/models/employee.js
@@ -1,3 +1,22 @@
 module.exports = function(Employee) {
+	Employee.getName = function(id, cb){
+		Employee.findOne({fields: {name: true}, where:{id:id}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					cb(null,instance);
+				}
+			});
+	};
 
+	Employee.remoteMethod(
+		'getName',
+		{
+			accepts: {arg: 'id', type: 'string'},
+			returns: {arg: 'name', type: 'string', root: true},
+			http: {path: '/getName', verb: 'get', source: 'query'},
+			description: "Get employee name by id"
+		}
+	);
 };
