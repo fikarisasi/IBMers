4aea4728ae5ebce9767aed7e443a62e2bc93a807
Muhammad Zulfikar <fikarisasi@gmail.com>
2016-08-19 17:46:31 +0700

    update getPostByRole based on true or false attribute div_jti and div_gbs

--- common/models/post.js
+++ common/models/post.js
@@ -1,6 +1,34 @@
+// save old method to getPostByRole
+// module.exports = function(Post) {
+// 	Post.getPostByRole = function(role,cb){
+// 		Post.find({where: {receiver: {like: role}}},
+// 			function(err,instance){
+// 				if(instance===null){
+// 					cb(null,null);
+// 				}else{
+// 					cb(null,instance);
+// 				}
+// 			});
+// 	};
+
+// 	Post.remoteMethod(
+// 		'getPostByRole',
+// 		{
+// 			accepts: {arg: 'role', type: 'string'},
+// 			returns: {
+// 				arg: 'id', type: 'string', root: true
+// 			},
+// 			http: {path: '/getPostByRole', verb: 'get', source: 'query'},
+// 			description: "Get all posts by user role"
+// 		}
+// 	);
+// };
+
 module.exports = function(Post) {
 	Post.getPostByRole = function(role,cb){
-		Post.find({where: {receiver: {like: role}}},
+		console.log(role === "div_jti");
+		if(role === "div_jti"){
+			Post.find({where: {div_jti: true}},
 			function(err,instance){
 				if(instance===null){
 					cb(null,null);
@@ -8,6 +36,17 @@ module.exports = function(Post) {
 					cb(null,instance);
 				}
 			});
+		}else if(role === "div_gbs"){
+			Post.find({where: {div_gbs: true}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					cb(null,instance);
+				}
+			});
+		}
+		
 	};
 
 	Post.remoteMethod(
