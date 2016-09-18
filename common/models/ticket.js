87e028a35c925abb8fbb8f82ad631fd7d306a948
Muhammad Zulfikar <fikarisasi@gmail.com>
2016-09-18 18:45:08 +0700

    add createTicket method

--- common/models/ticket.js
+++ common/models/ticket.js
@@ -1,3 +1,38 @@
 module.exports = function(Ticket) {
+	Ticket.createTicket = function(data, cb){
+		Ticket.create(data,
+			function(err, instance){
+				if(instance===null){
+					cb(null, null);
+				}else {
+					var ticketId = instance['id'];
+					Ticket.updateAll({id: ticketId}, {qrcode_link: "http://api.qrserver.com/v1/create-qr-code/?data=" + ticketId},
+						function(err, instance){
+							if(instance===null){
+								cb(null,null);
+							}else {
+								Ticket.findById(ticketId, 
+									function(err, instance){
+										if(instance===null){
+											cb(null,null);
+										}else {
+											cb(null, instance);
+										}
+									})
+							}
+						});
+				}
+			})
+		
+	}
 
+	Ticket.remoteMethod(
+		'createTicket',
+		{
+			accepts: {arg: 'data', type: 'object', http: {source: 'body'}},
+			returns: {type: 'string', root: 'true'},
+			http: {path: '/createTicket', verb: 'post', source: 'query'},
+			description: "Create a ticket for specific event"
+		}
+	);
 };
