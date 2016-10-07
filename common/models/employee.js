a1f52165d2a773e2078929dfc11f5d5775a4ab2f
destantifw <destantifw82@gmail.com>
2016-10-07 08:17:04 +0700

    add photo to message.json and photo to method getContacts

--- common/models/employee.js
+++ common/models/employee.js
@@ -22,7 +22,7 @@ module.exports = function(Employee) {
 	};
 
 	Employee.getContacts = function(cb){
-		Employee.find({fields: {id:true, username: true, name: true, division:true}},
+		Employee.find({fields: {id:true, username: true, name: true, division:true, photo:true}},
 			function(err,instance){
 				if(instance===null){
 					cb(null,null);
